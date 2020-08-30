60a354386803926c2b1b537857262e6797f38271
localhost-0001 <37961130+localhost-0001@users.noreply.github.com>
2020-08-30 04:56:08 +0200

    y
    dying is hard

---
+++ ekan.c
@@ -0,0 +1,144 @@
+//known_bugs: snake_tp, snake_input, no deathchecks just yet
+//stupidly overengineered
+
+#include<stdio.h>
+#include<ncurses.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<time.h>
+
+const int HeaderTest = 1;
+enum DIRECTION  { UP , DOWN , LEFT , RIGHT };
+
+void moveSnake(enum DIRECTION CurrentDirection, int **snek);
+void generateFood(int *a, int my, int mx);
+
+int main(){
+	initscr();	
+	nodelay(stdscr, 1);	
+	curs_set(FALSE);
+	keypad(stdscr, 1);
+	refresh();
+	sleep(1);
+
+	bool active = 1;
+	bool FoodEaten = 0;
+
+	int food[] = {0, 0};	
+	int SnakeArraySize = 10;
+	int SnakeLength = 9;
+	int len = sizeof(int*) * SnakeArraySize + sizeof(int[2]) * SnakeArraySize;
+	int **snake = (int **)malloc(len);
+	int *SnakePTR = (int *)(snake + SnakeArraySize);
+
+	enum DIRECTION CurrentDirection = RIGHT;
+
+	for(int i = 0; i < SnakeArraySize; i++){
+		snake[i] = (SnakePTR + 2 * i);
+	}
+
+	snake[0][0] = 1;
+	snake[0][1] = 11;
+	snake[1][0] = 1;
+	snake[1][1] = 10;
+	snake[2][0] = 1;
+	snake[2][1] = 9;
+	snake[3][0] = 1;
+	snake[3][1] = 8;
+
+	noecho();
+	int my, mx;
+	while(active){
+		//figure out screen size
+		getmaxyx(stdscr, my, mx);
+		clear();
+		int ch = wgetch(stdscr);
+		for(int i = 0; i < SnakeLength - 5; i++){
+
+			int sy = snake[i][0];
+			int sx = snake[i][1];
+			mvprintw(sy, sx, "X"); 
+		}
+		//check if player ded
+		int sneky = snake[0][0];
+		int snekx = snake[0][1];
+		//TODO: COMPLETE DEATHCHECKS;
+		if (sneky == my || snekx == mx || snekx < 0 || sneky < 0){
+			active = 0;	exit(12);	/*return 0;*/}
+		if((sneky == food[0] && snekx == food[1]) || !food[0]){
+		//yey, food eaten
+			generateFood(food, my, mx);
+			FoodEaten = 1;
+			SnakeLength++;
+		}	
+		
+		if(ch == KEY_UP && CurrentDirection != DOWN){
+			CurrentDirection = UP;	
+		} else if(ch == KEY_DOWN && CurrentDirection != UP){
+			CurrentDirection = DOWN;
+		} else if (ch == KEY_LEFT && CurrentDirection != RIGHT){
+			CurrentDirection = LEFT;
+		} else if (ch == KEY_RIGHT && CurrentDirection != LEFT){
+			CurrentDirection = RIGHT;
+		}
+		
+		if(SnakeArraySize <= SnakeLength - 2){
+			SnakeArraySize = SnakeArraySize + 8;
+			int newlen = sizeof(int*) * (SnakeArraySize) + sizeof(int[2]) * (SnakeArraySize);
+			int **NewArray = realloc(snake, newlen);
+			if(NewArray){
+				SnakePTR = (int *)(NewArray + SnakeArraySize);
+		
+			for(int i = 0; i < SnakeArraySize; i++){
+				NewArray[i] = (SnakePTR + 2 * i);
+			}
+			snake = NewArray;
+		} else {exit(19);}
+		//free(NewArray);
+		} 	
+		for (int i = SnakeLength - 2; i > 0; i--){
+			snake[i][0] = snake[i - 1][0];
+			snake[i][1] = snake[i - 1][1];
+	}
+		
+		FoodEaten = 0;
+		moveSnake(CurrentDirection, snake);
+		mvprintw(--my, --mx, "m");	
+		mvprintw(food[0], food[1], "%c", ACS_PI);
+		refresh(); 
+		usleep(100000);
+
+	}
+	usleep(1410065407);
+	return 0;
+}
+
+void generateFood(int *a, int my, int mx){
+	srand(time(NULL));	
+	a[1] = (rand() % mx) + 1;
+	a[0] = (rand() % my) + 1;
+	return;
+}
+
+void moveSnake(enum DIRECTION CurrentDirection, int **snek){
+	snek[0][0] = snek[1][0];
+	snek[0][1] = snek[1][1];
+	switch(CurrentDirection){
+		case UP:
+			//snek[0][0]--;
+			snek[0][0] = snek[0][0] - 1;
+			break;
+		case DOWN:
+			//snek[0][0]++;
+			snek[0][0] = snek[0][0] + 1;
+			break;
+		case LEFT:
+			//snek[0][1]--;
+			snek[0][1] = snek[0][1] - 1;
+			break;
+		case RIGHT:
+			//snek[0][1]++;
+			snek[0][1] = snek[0][1] + 1;
+			break;
+	}
+}
---
+++ makefile
@@ -0,0 +1,34 @@
+# change application name here (executable output name)
+TARGET=ekanc
+
+# compiler
+CC=$(CROSS)gcc
+# debug
+DEBUG=-g
+# optimisation
+OPT=-O2
+# warnings
+WARN=-Wall
+
+AR=$(CROSS)ar
+
+PTHREAD=-pthread
+
+CCFLAGS=$(DEBUG) $(OPT) $(WARN) $(PTHREAD) -pipe -fno-exceptions -ffunction-sections -fdata-sections -flto
+
+GTKLIB=`$(CROSS)pkg-config --cflags --libs gtk+-3.0` 
+
+# linker
+LD=$(CROSS)gcc
+LDFLAGS=$(PTHREAD) $(GTKLIB) -lncurses -ltinfo -lpthread -lcurses
+
+OBJS=	ekan.o
+
+all: $(OBJS)
+	$(LD) -o $(TARGET) $(OBJS) $(LDFLAGS)
+    
+ekan.o: ekan.c
+	$(CC) -c $(CCFLAGS) ekan.c $(GTKLIB) -o ekan.o
+    
+clean:
+	rm -f *.o $(TARGET)
